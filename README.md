6999e5f94c12481dd357ed4f95eddd5f24ffb2bb
reganbrown <regan_brown@icloud.com>
2024-10-06 10:15:18 -0300

    add env files for data

---
+++ README.md
@@ -0,0 +1,3 @@
+# BrainFlix-API
+
+Instructions: edit the .env.example file to be a .env file, use the default values set there or change to your own preferences. In the future, space for sensitive data like API keys will also be included in the example file with no default value, and they will need to be retrieved with additional steps that will be included in the README files.
--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import videoRoutes from "./routes/videos.js";
 const app = express();
+import "dotenv/config";
+
+const { PORT, BACKEND_URL } = process.env;
 
 app.use(cors());
 app.use(express.static("public"));
 
 app.use("/videos", videoRoutes);
 
-app.listen(8080, function () {
-  console.log("listening on port 8080");
+app.listen(PORT, function () {
+  console.log(`listening on port ${PORT}`);
 });
